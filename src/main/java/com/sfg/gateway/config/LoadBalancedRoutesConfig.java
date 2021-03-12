c33e893807ef4357d19357e7b635a864253a0380
Faiz Kidwai <f.y.kidwai@gmail.com>
2021-03-12 23:15:07 +0530

    config res4j for gateway circuit breaker. closes #6

--- src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
@@ -16,7 +16,13 @@ public class LoadBalancedRoutesConfig {
             .route("beer-service",
                 r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*").uri("lb://beer-service"))
             .route("order-service", r -> r.path("/api/v1/customers/**").uri("lb://order-service"))
-            .route("inventory-service", r -> r.path("/api/v1/beer/*/inventory").uri("lb://inventory-service")).build();
+            .route("inventory-service",
+                r -> r.path("/api/v1/beer/*/inventory")
+                    .filters(f -> f.circuitBreaker(
+                        c -> c.setName("inventoryCB").setFallbackUri("forward:/inventory-failover").setRouteId("inv-route")))
+                    .uri("lb://inventory-service"))
+            .route("inventory-failover-service", r -> r.path("/inventory-failover/**").uri("lb://inventory-failover"))
+            .build();
     }
 
 }
